1645b7e35bfd170d10d306d2229b65910775bdcc
Leicher Julien <julien@sky.thinkpad>
2018-05-01 20:05:26 +0200

    Updated sample

--- example/skills/lights/lights.py
+++ example/skills/lights/lights.py
@@ -1,6 +1,6 @@
 from atlas_sdk import SkillClient, Intent, Slot, Request
 
-def turn_lights(request):
+def turn_lights(request, switch):
   """
   :type request: Request
 
@@ -15,7 +15,13 @@ def turn_lights(request):
   if type(rooms) is not list:
     rooms = [rooms]
 
-  request.show(_('Turning lights on in %s') % ', '.join(rooms), terminate=True) # pylint: disable=E0602
+  request.show(_('Turning lights %s in %s') % (switch, ', '.join(rooms)), terminate=True) # pylint: disable=E0602
+
+def turn_lights_on(request):
+  turn_lights(request, 'on')
+
+def turn_lights_off(request):
+  turn_lights(request, 'off')
 
 if __name__ == '__main__':
   lights_skill = SkillClient(
@@ -24,7 +30,8 @@ if __name__ == '__main__':
     author='Julien LEICHER',
     description='Lights skill example',
     intents=[
-      Intent('sampleTurnOnLight', turn_lights, [Slot('room')])
+      Intent('turnLightOn', turn_lights_on, [Slot('room')]),
+      Intent('turnLightOff', turn_lights_off, [Slot('room')]),
     ],
   )
 
--- example/skills/weather/weather.py
+++ example/skills/weather/weather.py
@@ -8,18 +8,19 @@ def get_forecasts(request):
 
   """
 
-  date = request.slot('weatherDate', converter=dateParse)
+  date = request.slot('date', converter=dateParse)
 
   if not date:
-    return request.ask('weatherDate', _('For when do you want the forecast?')) # pylint: disable=E0602
+    return request.ask('date', _('For when do you want the forecast?')) # pylint: disable=E0602
 
-  location = request.slot('weatherLocation')
+  location = request.slot('city')
 
   if not location:
-    return request.ask('weatherLocation', _('For where do you want the forecast?')) # pylint: disable=E0602
+    return request.ask('city', _('For where do you want the forecast?')) # pylint: disable=E0602
 
   time.sleep(3)
 
+  # Do something with the key
   api_key = request.env('WEATHER_API_KEY') # pylint: disable=W0612
 
   request.show(_("Well, I'll try to find the forecasts for %s on %s") % (location, date), terminate=True)  # pylint: disable=E0602
@@ -31,7 +32,7 @@ if __name__ == '__main__':
     author='Julien LEICHER',
     description='Gives some weather forecasts',
     intents=[
-      Intent('sampleGetWeather', get_forecasts, [Slot('weatherDate'), Slot('weatherLocation')])
+      Intent('weatherForecast', get_forecasts, [Slot('date'), Slot('city')])
     ],
     env=[Env('WEATHER_API_KEY')]
   )
